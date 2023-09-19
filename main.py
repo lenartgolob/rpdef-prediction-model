2217f210988cbdd460063f385cdd8e653dded598
Lenart GOlob <lenart.golob17@gmail.com>
2023-09-19 16:11:03 +0200

    new prediction model

--- main.py
+++ main.py
@@ -58,41 +58,106 @@ def get_train_data():
 #     average_train = sum(average_baskets_per_season) / len(average_baskets_per_season)
 #     return predicted_average_22_23_season
 
+def get_predictions_for_latest_season():
+    # Get the training data (assuming get_train_data() returns X_train, y_train)
+    X_train, y_train = get_train_data()
+    X_train = np.array(X_train).reshape(-1, 1)
+    # Calculate the number of years in your training data
+    num_years = len(X_train) // 30  # Assuming 30 values per year
 
-# Get the training data (assuming get_train_data() returns X_train, y_train)
-X_train, y_train = get_train_data()
-X_train = np.array(X_train).reshape(-1, 1)
-# Calculate the number of years in your training data
-num_years = len(X_train) // 30  # Assuming 30 values per year
+    # Add the year as a feature to the training data
+    years_train = np.array([year for year in range(2013, 2013 + num_years)]).reshape(-1, 1)
 
-# Add the year as a feature to the training data
-years_train = np.array([year for year in range(2013, 2013 + num_years)]).reshape(-1, 1)
+    # Duplicate the years to match the number of data points per year
+    years_train = np.repeat(years_train, 30, axis=0)
 
-# Duplicate the years to match the number of data points per year
-years_train = np.repeat(years_train, 30, axis=0)
+    # Create and train a linear regression model
+    model = LinearRegression()
+    model.fit(np.hstack((X_train, years_train)), y_train)
 
-# Create and train a linear regression model
-model = LinearRegression()
-model.fit(np.hstack((X_train, years_train)), y_train)
+    # Get the data for the 2022/23 season
+    X_test, _ = get_data_year("22/23")
+    X_test = np.array(X_test).reshape(-1, 1)
+    # Add the year as a feature for the test data
+    num_test_samples = len(X_test)
+    years_test = np.array([2023] * num_test_samples).reshape(-1, 1)
 
-# Get the data for the 2022/23 season
-X_test, _ = get_data_year("22/23")
-X_test = np.array(X_test).reshape(-1, 1)
-# Add the year as a feature for the test data
-num_test_samples = len(X_test)
-years_test = np.array([2023] * num_test_samples).reshape(-1, 1)
+    # Combine the test data with the year feature
+    X_test_with_year = np.hstack((X_test, years_test))
 
-# Combine the test data with the year feature
-X_test_with_year = np.hstack((X_test, years_test))
+    # Make predictions for the 2022/23 season
+    predictions = model.predict(X_test_with_year)
 
-# Make predictions for the 2022/23 season
-predictions = model.predict(X_test_with_year)
+    # Evaluate the model's performance
+    team_defenses = pd.read_csv('./team-defenses/team_defenses_22_23.csv')
+    team_defenses = team_defenses.sort_values(by='Team', ascending=True)
+    y_test = team_defenses["DEF"].values.tolist()
+    mae = mean_absolute_error(y_test, predictions)
+    print(f"Mean Absolute Error: {mae}")
+    print(y_test)
+    print(predictions)
 
-# Evaluate the model's performance
-team_defenses = pd.read_csv('./team-defenses/team_defenses_22_23.csv')
-team_defenses = team_defenses.sort_values(by='Team', ascending=True)
-y_test = team_defenses["DEF"].values.tolist()
-mae = mean_absolute_error(y_test, predictions)
-print(f"Mean Absolute Error: {mae}")
-print(y_test)
-print(predictions)
+def get_predictions_based_on_old_RPDEF():
+    # Get the training data (assuming get_train_data() returns X_train, y_train)
+    X_train, y_train = get_train_data()
+    X_train = np.array(X_train).reshape(-1, 1)
+    # Calculate the number of years in your training data
+    num_years = len(X_train) // 30  # Assuming 30 values per year
+
+    # Add the year as a feature to the training data
+    years_train = np.array([year for year in range(2013, 2013 + num_years)]).reshape(-1, 1)
+
+    # Duplicate the years to match the number of data points per year
+    years_train = np.repeat(years_train, 30, axis=0)
+
+    # Create and train a linear regression model
+    model = LinearRegression()
+    model.fit(np.hstack((X_train, years_train)), y_train)
+
+    # Get the data for the 2022/23 season
+    X_test, _ = get_data_for_new_season()
+    X_test = np.array(X_test).reshape(-1, 1)
+    # Add the year as a feature for the test data
+    num_test_samples = len(X_test)
+    years_test = np.array([2023] * num_test_samples).reshape(-1, 1)
+
+    # Combine the test data with the year feature
+    X_test_with_year = np.hstack((X_test, years_test))
+
+    # Make predictions for the 2022/23 season
+    predictions = model.predict(X_test_with_year)
+
+    # Evaluate the model's performance
+    team_defenses = pd.read_csv('./team-defenses/team_defenses_22_23.csv')
+    team_defenses = team_defenses.sort_values(by='Team', ascending=True)
+    y_test = team_defenses["DEF"].values.tolist()
+    mae = mean_absolute_error(y_test, predictions)
+    print(f"Mean Absolute Error: {mae}")
+    print(y_test)
+    print(predictions)
+
+def get_data_for_new_season():
+    X = []
+    y = []
+    data = json.load(open('db.json'))
+    mydb = mysql.connector.connect(
+        host=data["host"],
+        user=data["user"],
+        password=data["password"],
+        port=data["port"],
+        database=data["database"],
+    )
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute(f"SELECT AVG(P21.DEF) AS RPDEF_21_22 FROM Player AS P22 JOIN Player AS P21 ON P22.Player = P21.Player WHERE P22.SeasonYear = '22/23' AND P21.SeasonYear = '21/22' AND P21.DEF > 0 GROUP BY P22.Team ORDER BY P22.Team")
+    ratings = mycursor.fetchall()
+    for rating in ratings:
+        X.append(rating["RPDEF_21_22"])
+
+    team_defenses = pd.read_csv('./team-defenses/team_defenses_22_23.csv')
+    team_defenses = team_defenses.sort_values(by='Team', ascending=True)
+    y.extend(team_defenses["DEF"].values.tolist())
+    return (X, y)
+
+get_predictions_for_latest_season()
+print("--------------------------------")
+get_predictions_based_on_old_RPDEF()
